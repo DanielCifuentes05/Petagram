13c0a29bcb7d7907b240048fb325f0740132d7f0
Daniel Cifuentes <54119708+DanielCifuentes05@users.noreply.github.com>
2020-07-11 18:16:47 -0500

    Actualizacion reciclerview
    reciclerview, actionbar, up

--- Petagram/app/src/main/java/com/daniel/petagram/MainActivity.java
+++ Petagram/app/src/main/java/com/daniel/petagram/MainActivity.java
@@ -1,13 +1,82 @@
 package com.daniel.petagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Mascota> mascotas;
+    private RecyclerView listaMascotas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mytoolbar=(Toolbar) findViewById(R.id.miToolbar);
+        setSupportActionBar(mytoolbar);
+
+        ImageButton cinco = (ImageButton) findViewById(R.id.cincoMejores);
+
+        listaMascotas=(RecyclerView) findViewById(R.id.rvMascotas);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        listaMascotas.setLayoutManager(llm);
+        llenarLista();
+        inicializarAdapter();
+        myFab();
+
+        cinco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Cinco", Toast.LENGTH_SHORT).show();
+                Intent intent= new Intent(MainActivity.this, mejores5.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void inicializarAdapter(){
+        MascotaAdapter adapter = new MascotaAdapter(mascotas,this);
+        listaMascotas.setAdapter(adapter);
+
+    }
+
+    public void llenarLista(){
+
+        mascotas = new ArrayList<>();
+
+        mascotas.add(new Mascota("Lupe","Gato","Hembra",3, R.drawable.gato3));
+        mascotas.add(new Mascota("Max","Perro", "Macho",5, R.drawable.mascota_mosquito_1));
+        mascotas.add(new Mascota("Rufo","Perro", "Macho",7, R.drawable.perrito1));
+        mascotas.add(new Mascota("Hana","Perro", "Hembra",6, R.drawable.perro));
+        mascotas.add(new Mascota("Fiona","Perro", "Hembra",9, R.drawable.perro6));
+        mascotas.add(new Mascota("Aron","Gato", "Macho",6, R.drawable.gato));
+    }
+
+    public void myFab(){
+        FloatingActionButton Fab = (FloatingActionButton) findViewById(R.id.myFAB);
+        Fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), getResources().getString(R.string.toast), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ Petagram/app/src/main/java/com/daniel/petagram/mejores5.java
@@ -0,0 +1,50 @@
+package com.daniel.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class mejores5 extends AppCompatActivity {
+
+    ArrayList<Mascota> mascotas5;
+    private RecyclerView listaMascotas;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mejores5);
+
+        Toolbar mytoolbar=(Toolbar) findViewById(R.id.miToolbar2);
+        setSupportActionBar(mytoolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        listaMascotas=(RecyclerView) findViewById(R.id.rvMascotas2);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        listaMascotas.setLayoutManager(llm);
+        llenarLista();
+        inicializarAdapter();
+    }
+
+    public void inicializarAdapter(){
+        MascotaAdapter adapter = new MascotaAdapter(mascotas5,this);
+        listaMascotas.setAdapter(adapter);
+
+    }
+    public void llenarLista(){
+
+        mascotas5 = new ArrayList<>();
+
+        //mascotas.add(new Mascota("Lupe","Gato","Hembra",3, R.drawable.gato3));
+        mascotas5.add(new Mascota("Max","Perro", "Macho",5, R.drawable.mascota_mosquito_1));
+        mascotas5.add(new Mascota("Rufo","Perro", "Macho",7, R.drawable.perrito1));
+        mascotas5.add(new Mascota("Hana","Perro", "Hembra",6, R.drawable.perro));
+        mascotas5.add(new Mascota("Fiona","Perro", "Hembra",9, R.drawable.perro6));
+        mascotas5.add(new Mascota("Aron","Gato", "Macho",6, R.drawable.gato));
+    }
+
+}
